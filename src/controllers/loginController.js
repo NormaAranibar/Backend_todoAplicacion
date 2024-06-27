52564cb8ddef51d54b7b12a02243e304e00c1928
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-06-26 22:13:48 -0500

    loginController modified

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -19,7 +19,8 @@ const loginController = async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    return res.status(200).json({ message: "Login successful" });
+    // return res.status(200).json({ message: "Login successful" });
+    return res.status(200).json(user);
   } catch (error) {
     console.error("Error during login:", error);
     return res.status(500).json({ message: "Internal server error" });
