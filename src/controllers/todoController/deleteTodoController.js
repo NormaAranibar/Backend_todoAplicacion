be842833623231523274257c956769adea7b4b20
Leodan Condori <67523914+LeodanCondori7000@users.noreply.github.com>
2024-06-27 17:00:37 -0500

    delete and update controllers implemented

---
+++ src/controllers/todoController/deleteTodoController.js
@@ -0,0 +1,26 @@
+const { Todo } = require("../../db.js"); // Assuming Todo model from your database
+
+const deleteTodoController = async (req, res) => {
+  const { idTodo } = req.params;
+
+  try {
+    // // Check if todo exists
+    // const todo = await Todo.findByPk(idTodo);
+    // if (!todo) {
+    //   return res.status(404).json({ message: "Todo not found" });
+    // }
+
+    await Todo.destroy({
+      where: {
+        id: idTodo,
+      },
+    });
+
+    return res.status(200).json({ message: "Todo deleted successfully" });
+  } catch (error) {
+    console.error("Error deletting todo:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+module.exports = deleteTodoController;
--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -4,11 +4,17 @@ const getTodosController = require("../controllers/todoController/getTodosContro
 const getOneTodoController = require("../controllers/todoController/getOneTodoController.js");
 const getTodosById = require("../controllers/todoController/getTodosById.js");
 const createTodoController = require("../controllers/todoController/createTodoController.js");
+const updateTodoController = require("../controllers/todoController/updateTodoController.js");
+const deleteTodoController = require("../controllers/todoController/deleteTodoController.js");
 
 const router = Router();
 
 router.route("/todos").get(getTodosController).post(createTodoController);
-router.route("/todos/:idTodo").get(getOneTodoController);
+router
+  .route("/todos/:idTodo")
+  .get(getOneTodoController)
+  .put(updateTodoController)
+  .delete(deleteTodoController);
 router.route("/todos/user/:idUser").get(getTodosById);
 
 module.exports = router;
